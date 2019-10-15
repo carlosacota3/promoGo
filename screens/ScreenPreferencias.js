41a35e4174088cad170415161ac6dd5bff162f14
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-10-14 20:38:41 -0600

    Pantalla de preferencias

--- screens/ScreenPreferencias.js
+++ screens/ScreenPreferencias.js
@@ -15,39 +15,28 @@ class ScreenPreferencias extends Component {
             centerComponent={{ text: 'Preferencias', style: { color: '#fff', fontSize: 24, fontWeight: 'bold'}}}
             // rightComponent={{ icon: 'home', color: '#fff' }}
         />
-             <ListItem>
+             <ScrollView>
                 <View style={styles.container}>
                     <Text style={styles.titulos}>Comida</Text>
-                    {/*  */}
                     <CheckBox title='Pizza' checked={this.state.ch_pizza} onPress={() => this.setState({ch_pizza: !this.state.ch_pizza})}/>
-                    {/*  */}
                     <CheckBox title='Hamburguesa' checked={this.state.ch_hamburguesa} onPress={() => this.setState({ch_hamburguesa: !this.state.ch_hamburguesa})}/>
-                    {/*  */}
                     <CheckBox title='Nachos' checked={this.state.ch_nachos} onPress={() => this.setState({ch_nachos: !this.state.ch_nachos})}/>
-                    {/*  */}
                     <CheckBox title='Boneless' checked={this.state.ch_boneless} onPress={() => this.setState({ch_boneless: !this.state.ch_boneless})}/>
-                    {/*  */}
                     <CheckBox title='Hotdogs' checked={this.state.ch_hotdogs} onPress={() => this.setState({ch_hotdogs: !this.state.ch_hotdogs})}/>
-                    {/*  */}
                     <CheckBox title='Sushi' checked={this.state.ch_sushi} onPress={() => this.setState({ch_sushi: !this.state.ch_sushi})}/>
                 </View>
                 <View style={styles.container}>
                     <Text style={styles.titulos}>Ropa</Text>
-                    <CheckBox title='Camisas' checked={this.state.ch_pizza} onPress={() => this.setState({ch_pizza: !this.state.ch_pizza})}/>
-                    {/*  */}
-                    <CheckBox title='Pantalones' checked={this.state.ch_pizza} onPress={() => this.setState({ch_pizza: !this.state.ch_pizza})}/>
-                    {/*  */}
-                    <CheckBox title='Vestidos' checked={this.state.ch_pizza} onPress={() => this.setState({ch_pizza: !this.state.ch_pizza})}/>
-                    {/*  */}
-                    <CheckBox title='Calzado' checked={this.state.ch_pizza} onPress={() => this.setState({ch_pizza: !this.state.ch_pizza})}/>
-                    {/*  */}
-                    <CheckBox title='Accesorios' checked={this.state.ch_pizza} onPress={() => this.setState({ch_pizza: !this.state.ch_pizza})}/>
-                    {/*  */}
+                    <CheckBox title='Camisas' checked={this.state.ch_camisa} onPress={() => this.setState({ch_camisa: !this.state.ch_camisa})}/>
+                    <CheckBox title='Pantalones' checked={this.state.ch_pantalones} onPress={() => this.setState({ch_pantalones: !this.state.ch_pantalones})}/>
+                    <CheckBox title='Vestidos' checked={this.state.ch_vestidos} onPress={() => this.setState({ch_vestidos: !this.state.ch_vestidos})}/>
+                    <CheckBox title='Calzado' checked={this.state.ch_calzado} onPress={() => this.setState({ch_calzado: !this.state.ch_calzado})}/>
+                    <CheckBox title='Accesorios' checked={this.state.ch_accesorios} onPress={() => this.setState({ch_accesorios: !this.state.ch_accesorios})}/>
                 </View>
                 <View style={styles.container}>
                     <Text style={styles.titulos}>Otros</Text>
                 </View>
-                </ListItem>
+                </ScrollView>
       </View>
     );
   }
